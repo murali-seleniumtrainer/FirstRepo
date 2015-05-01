b34b01723bc16a103d5d56757ea649f3a48decda
MuralidharG <muralidhar.gude@ngahr.com>
2015-05-01 13:58:17 +0530

    test project

---
+++ myproject/src/test/java/com/mylearning/myproject/TestCase1.java
@@ -0,0 +1,12 @@
+package com.mylearning.myproject;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("running testng from maven");
+	  
+  }
+}
